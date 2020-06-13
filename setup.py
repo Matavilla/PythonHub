c6692ad36a1a464af2ec92d97e79bbf7af0e166d
vultar150 <vultar1500@gmail.com>
2020-06-13 22:03:24 +0300

    delete email

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 
     # metadata to display on PyPI
     author="Владимир Шапошников, Владимир Рябченков, Андрей Морквин",
-    author_email="me@example.com",
+    #author_email="me@example.com",
     description="This is LifeHub package",
     url="https://github.com/Matavilla/LifeHub",  # project home page, if any
     # project_urls={
